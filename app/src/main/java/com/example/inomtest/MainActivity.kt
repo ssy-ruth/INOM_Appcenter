cff1aecead5da9743508dea651e73cb0699ec024
apricot <ruth1442@naver.com>
2022-01-17 17:08:51 +0900

    create branch

--- app/src/main/java/com/example/inomtest/MainActivity.kt
+++ app/src/main/java/com/example/inomtest/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.inomtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = nav_host_fragment.findNavCotroller()
     }
 }
