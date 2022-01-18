65e05f16c546cfce19ccda30ba58049ad00479ad
jjh2x <jjh3706@gmail.com>
2022-01-18 23:30:45 +0900

    fragment_home 까지 프래그먼트 전환 구현.

--- app/src/main/java/com/example/inomtest/fragment/SignupFinishFragment.kt
+++ app/src/main/java/com/example/inomtest/fragment/SignupFinishFragment.kt
@@ -1,32 +1,26 @@
 package com.example.inomtest.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.inomtest.R
+import com.example.inomtest.databinding.FragmentSignupFinishBinding
+import com.example.inomtest.databinding.FragmentSignupIdBinding
+import com.example.inomtest.databinding.FragmentSignupNameBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignupFinishFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignupFinishFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentSignupFinishBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,26 +28,32 @@ class SignupFinishFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_finish, container, false)
+        _binding = FragmentSignupFinishBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signupFinishBtn.setOnClickListener{
+            Log.d("buttonTest", "버튼은 잘 눌림!!!!!!!")
+            it.findNavController().navigate(R.id.action_signupFinishFragment_to_fragment_home)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignupFinishFragment.
-         */
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             SignupFinishFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
