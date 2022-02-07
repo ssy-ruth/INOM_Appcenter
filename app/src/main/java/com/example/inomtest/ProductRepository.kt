7c69e4ba1e750c3802e22f95a1aaa3303ba43a43
jjh2x <jjh3706@gmail.com>
2022-02-07 21:16:04 +0900

    통신 CLEARTEXT communication to [url] not permitted by network security policy 해결중

--- app/src/main/java/com/example/inomtest/ProductRepository.kt
+++ app/src/main/java/com/example/inomtest/ProductRepository.kt
@@ -1,5 +1,6 @@
 package com.example.inomtest
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.example.inomtest.dataClass.Data
 import com.example.inomtest.dataClass.ProductItem
@@ -15,7 +16,7 @@ class ProductRepository {
 
     fun loadProductItems(page: Int) {
         // parameter["page"] = page.toString()
-        val call = InomApi.createApi().loadProducts(page.toString())
+        val call = InomApi.createApi().loadProducts("Bearer **", page.toString())
 
         call.enqueue(object : Callback<ProductItem> {
             override fun onResponse(
@@ -23,12 +24,17 @@ class ProductRepository {
                 response: Response<ProductItem>
             ) {
                 if (response.isSuccessful) {
+                    Log.d("결과1", "통신결과"+response.code().toString())
                     _products.value = response.body()!!.data
+                }
+
+                else {
+                    Log.d("결과2", "통신결과"+response.code().toString())
                 }
             }
 
             override fun onFailure(call: Call<ProductItem>, t: Throwable) {
-
+                Log.d("결과3", "통신결과: $t")
             }
         })
     }
--- app/src/main/java/com/example/inomtest/network/InomApiService.kt
+++ app/src/main/java/com/example/inomtest/network/InomApiService.kt
@@ -6,15 +6,19 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface InomApiService {
     @GET("/api/items?size=10&itemId=1000&categoryId=10&majorId=10&searchWord=10")
-    fun loadProducts(@Query("page") page: String): Call<ProductItem>
+    fun loadProducts(
+        @Header("Authorization") accessToken: String,
+        @Query("page") page: String): Call<ProductItem>
 }
 
 object InomApi {
-    private const val baseUrl = "https://inu-market.cf/"  // 베이스 URL
+    // private const val baseUrl = "https://inu-market.cf/"  // 베이스 URL
+    private const val baseUrl = "http://54.180.73.82:8081"
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
